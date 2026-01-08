660d6a4f122ee6384b9242c757993703b5becb09
Erfan <erfan.norozi54@gmail.com>
2026-01-08 18:18:50 +0330

    feat(navigation): disable default transitions in NavHost
    - Add EnterTransition and ExitTransition imports from Compose animation
    - Configure NavHost to use no transitions for enter, exit, pop-enter, and pop-exit animations
    - Improve navigation experience by preventing default transition animations between screens

--- app/src/main/java/com/voxcina/shop/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/voxcina/shop/presentation/navigation/NavGraph.kt
@@ -1,5 +1,7 @@
 package com.voxcina.shop.presentation.navigation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -180,7 +182,11 @@ fun NavGraph(
     NavHost(
         navController = navController,
         startDestination = Screen.Splash.route,
-        modifier = modifier
+        modifier = modifier,
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None }
     ) {
         composable(route = Screen.Splash.route) {
             SplashScreen(
